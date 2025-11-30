cbbdb9b6c2a204ae6db609d7c7d95628a186b47a
kaushik <kaushikwalde2026@gmail.com>
2025-11-30 15:13:49 +0530

    Connected database and added tables

--- src/main/java/com/backendlld/productservice_1/models/BaseModel.java
+++ src/main/java/com/backendlld/productservice_1/models/BaseModel.java
@@ -1,5 +1,9 @@
 package com.backendlld.productservice_1.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +11,13 @@ import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private Date created_at;
     private Date updated_at;
 
--- src/main/java/com/backendlld/productservice_1/models/Product.java
+++ src/main/java/com/backendlld/productservice_1/models/Product.java
@@ -1,14 +1,20 @@
 package com.backendlld.productservice_1.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name = "products")
 public class Product extends BaseModel{
     private String productName;
     private String productDescription;
     private double productPrice;
     private String image;
+
+
+    @ManyToOne
     private Category category;
 }
